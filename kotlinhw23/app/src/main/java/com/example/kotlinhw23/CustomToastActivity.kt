75498d7207b928050dbe7601b8a3a07316f5c73f
110360105 <t110360105@ntut.org.tw>
2023-11-08 20:16:03 +0800

    Kotlin-HW2

---
+++ kotlinhw23/app/src/main/java/com/example/kotlinhw23/CustomToastActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinhw23
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomToastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.custom_toast)
+    }
+}
